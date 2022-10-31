d873dbc5d3eba9f54f3e607b15736c7a9a4d714c
wilsonTec <wilson.garcia@tecsup.edu.pe>
2022-10-31 13:36:41 -0500

    Initial Commit

---
+++ wilsonSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wilsonSnapchat
+//
+//  Created by Mac 18 on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
