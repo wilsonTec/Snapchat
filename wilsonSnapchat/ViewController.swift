8aa8fdb7eddca71d5881bb845402f8d78d98735e
wilsonTec <wilson.garcia@tecsup.edu.pe>
2022-10-31 16:06:23 -0500

    proyecto snb

--- wilsonSnapchat/ViewController.swift
+++ wilsonSnapchat/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
-class ViewController: UIViewController {
+class iniciarSesionViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
 }
 
